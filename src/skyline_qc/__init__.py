c9c9f860c69ce959d31785563da72b308d361056
thanadol-git <khunkoei@gmail.com>
2026-03-08 22:23:16 +0100

    check .csv output

---
+++ src/skyline_qc/__init__.py
@@ -0,0 +1,4 @@
+from .client import SkylineClient
+from .output_test import ImportFile
+
+__all__ = ["SkylineClient", "ImportFile"]
---
+++ src/skyline_qc/output_test.py
@@ -0,0 +1,57 @@
+import os
+import unittest
+import pandas as pd
+from .client import SkylineClient
+
+class ImportFile():
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+    
+    def import_skyline_file(self):
+        
+        # 1. Check if the file exists
+        if not os.path.exists(self.file_path):
+            raise FileNotFoundError(f"The file {self.file_path} does not exist.")
+        
+        # 2. Check file type
+        if not self.file_path.lower().endswith('.csv'):
+            raise ValueError("Provided file is not a CSV.")
+
+        # 3. Read the file
+        df = pd.read_csv(self.file_path)
+
+        # 4. Check if the file is empty
+        if df.empty:
+            raise ValueError(f"The file {self.file_path} is empty.")
+
+        # 5.  Check if the file has the expected columns
+        expected_columns = [
+            "Precursor",
+            "Replicate",
+            "File Name",
+            "Peptide Retention Time",
+            "Retention Time Calculator Score",
+            "Predicted Retention Time",
+            "Peptide Sequence",
+            "Peptide",
+            "Normalized Area",
+            "RatioLightToHeavy",
+            "Ratio Dot Product",
+            "Library Dot Product",
+            "Protein Name"
+        ]
+        
+        # 6. Check if the file has the expected columns
+        if not all(col in df.columns for col in expected_columns):
+            print(f"The file {self.file_path} does not have the expected columns. Expected:")
+            for col in expected_columns:
+                print(col)
+            raise ValueError(f"The file {self.file_path} does not have the expected columns.")
+        
+        # 7. Cross-check expected columns with actual column names from the file.
+        missing_columns = [col for col in expected_columns if col not in df.columns]
+        if missing_columns:
+            print(f"The following expected columns are missing from the file: {missing_columns}")
+            print(f"Actual columns in the file: {list(df.columns)}")
+            raise ValueError(f"The file {self.file_path} does not have the expected columns: {expected_columns}")
+        return df
