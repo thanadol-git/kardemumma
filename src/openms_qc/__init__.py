3a58610553fe24e889f9edbf842e37c4594fc998
thanadol-git <khunkoei@gmail.com>
2026-04-09 10:36:24 +0200

    add fns

---
+++ src/openms_qc/__init__.py
@@ -0,0 +1,13 @@
+from .openswath import (
+    import_openswath_file,
+    filter_best_peak_group,
+    plot_dotprod_kde,
+    calculate_ratio,
+)
+
+__all__ = [
+    "import_openswath_file",
+    "filter_best_peak_group",
+    "plot_dotprod_kde",
+    "calculate_ratio",
+]
